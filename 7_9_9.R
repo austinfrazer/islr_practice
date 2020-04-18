066c7f2a9833cef603949997f6c5144a4745602a
Rachel Frazer <austinfrazer@gmail.com>
2020-04-18 12:22:43 -0500

    did first half of number 9 in chapter 7

--- 7_9_9.R
+++ 7_9_9.R
@@ -2,6 +2,9 @@
 # to five Boston employment centers) and nox (nitrogen oxides concentration
 # in parts per 10 million) from the Boston data. We will treat
 # dis as the predictor and nox as the response.
+library(MASS)
+
+
 
 # (a) Use the poly() function to fit a cubic polynomial regression to
 # predict nox using dis. Report the regression output, and plot
@@ -9,7 +12,18 @@
 
 
 
+fit_nox3 = lm(nox ~ poly(dis, 3), data = Boston)
 
+dislims = range(Boston$dis)
+dis.grid = seq(from = dislims[1], to = dislims[2])
+preds = predict(fit_nox3, newdata = list(dis = dis.grid), se = TRUE)
+se.bands = cbind(preds$fit + 2 * preds$se.fit, preds$fit - 2 * preds$se.fit)
+
+par(mfrow = c(1, 2), mar = c(4.5, 4.5, 1, 1), oma = c(0, 0, 4, 0))
+plot(Boston$dis, Boston$nox, xlim = dislims, cex = .5, col = "darkgrey")
+title("Degree - 3 Polynomial", outer = T)
+lines(dis.grid, preds$fit, lwd = 2, col = "blue")
+matlines(dis.grid, se.bands, lwd = 1, col = "blue", lty = 3)
 
 
 # (b) Plot the polynomial fits for a range of different polynomial
