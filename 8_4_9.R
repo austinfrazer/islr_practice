587d6976b3f20a42c95015f6140ede403663c9e1
unknown <33237164+austinfrazer@users.noreply.github.com>
2020-05-24 18:29:48 -0500

    Finished 8_4_9.R.

--- 8_4_9.R
+++ 8_4_9.R
@@ -65,19 +65,33 @@ print(paste0('This variable is actually used:  ', summary_tree_OJ$used, "."))
 # What is the test error rate?
 
 
+pred_tree_OJ = predict(tree_OJ, OJ_test, type = "class")
+Purchase_test = OJ_test$Purchase
+my_tree  =  table(pred_tree_OJ, Purchase_test)
+print("below is my confusion matrix.")
+my_tree
+misclassification_rate = round((my_tree[1, 2] + my_tree[2, 1]) / sum(my_tree),3)
+print(paste0("My misclassification rate is:  ", misclassification_rate, "."))
+
 
 # (f) Apply the cv.tree() function to the training set in order to
 # determine the optimal tree size.
 
 
+set.seed(3)
+cv_OJ = cv.tree(tree_OJ, FUN = prune.tree)
 
 # (g) Produce a plot with tree size on the x-axis and cross-validated
 # classification error rate on the y-axis.
 
-
+plot(cv_OJ$size, cv_OJ$dev, type = "b", xlab = "Tree Size", ylab = "Deviance")
 
 # (h) Which tree size corresponds to the lowest cross-validated classification
 # error rate?
+
+
+print(paste0("The ideal number of terminal nodes ('size') is ", 
+             cv_OJ$size[which.min(cv_OJ$dev)], "."))
 
 
 
@@ -86,13 +100,36 @@ print(paste0('This variable is actually used:  ', summary_tree_OJ$used, "."))
 # to selection of a pruned tree, then create a pruned tree with five
 # terminal nodes.
 
-
+# Did not show any improvement by tuning, so going with 5.
+pruned_OJ = prune.tree(tree_OJ, best = 5)
 
 # (j) Compare the training error rates between the pruned and unpruned
 # trees. Which is higher?
+pruned_error_rate = summary(pruned_OJ)$misclass[1]/summary(pruned_OJ)$misclass[2]
+print(paste0("Pruned error rate is:  ", pruned_error_rate))
 
+unpruned_error_rate = summary(tree_OJ)$misclass[1]/summary(tree_OJ)$misclass[2]
+print(paste0("Unpruned error rate is:  ", unpruned_error_rate))
+
+if(pruned_error_rate < unpruned_error_rate){
+  print("Pruning offered an improvement in training error rates.")
+  }else{print("Pruning failed to offer any improvement in training error rates.")}
 
 
 # (k) Compare the test error rates between the pruned and unpruned
 # trees. Which is higher?
 
+pred_pruned_OJ = predict(pruned_OJ, OJ_test, type = "class")
+
+my_tree  =  table(pred_pruned_OJ, Purchase_test)
+print("below is my confusion matrix.")
+my_tree
+unpruned_misclassification_rate = misclassification_rate
+pruned_misclassification_rate = round((my_tree[1, 2] + my_tree[2, 1]) / sum(my_tree),3)
+
+if(pruned_misclassification_rate < unpruned_misclassification_rate){
+  print("Pruning offered an improvement in test error rates.")
+}else{print("Pruning failed to offer any improvement in test error rates.")}
+
+pruned_misclassification_rate
+unpruned_misclassification_rate
