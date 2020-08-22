b32b20f40338cdca8d553da8e9748a16708d37c5
unknown <33237164+austinfrazer@users.noreply.github.com>
2020-08-22 17:19:24 -0500

    created empty shell for 10_7_9

---
+++ 10_7_9.R
@@ -0,0 +1,27 @@
+# 9. Consider the USArrests data. We will now perform hierarchical clustering
+# on the states.
+
+
+
+# (a) Using hierarchical clustering with complete linkage and
+# Euclidean distance, cluster the states.
+
+
+
+# (b) Cut the dendrogram at a height that results in three distinct
+# clusters. Which states belong to which clusters?
+
+
+
+# (c) Hierarchically cluster the states using complete linkage and Euclidean
+# distance, after scaling the variables to have standard deviation
+# one.
+
+
+
+# (d) What effect does scaling the variables have on the hierarchical
+# clustering obtained? In your opinion, should the variables be
+# scaled before the inter-observation dissimilarities are computed?
+#   Provide a justification for your answer.
+
+
